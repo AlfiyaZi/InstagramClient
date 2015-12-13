7ec30ff9a9b02e604350d22dbc3737e9f3f4c803
Shehba Shahab <shehbashahab@gmail.com>
2015-12-13 11:10:46 -0800

    Lint fixes

--- app/src/main/java/com/codepath/instagramclient/activities/PhotosActivity.java
+++ app/src/main/java/com/codepath/instagramclient/activities/PhotosActivity.java
@@ -117,7 +117,6 @@ public class PhotosActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
         return super.onOptionsItemSelected(item);
     }
 }
