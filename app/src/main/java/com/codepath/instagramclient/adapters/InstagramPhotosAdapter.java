733ac0c7b1e814ae0d8dabc9717e30766c2ff3f0
Shehba Shahab <shehbashahab@gmail.com>
2016-01-23 23:50:24 -0800

    Improved performance with ViewHolder pattern

--- app/src/main/java/com/codepath/instagramclient/adapters/InstagramPhotosAdapter.java
+++ app/src/main/java/com/codepath/instagramclient/adapters/InstagramPhotosAdapter.java
@@ -28,57 +28,63 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
         InstagramPhoto photo = getItem(position);
 
-        // Check if we are using a recycled view, if not we need to inflate
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+
+            // Look up views to populate data
+            viewHolder.tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+            viewHolder.ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            viewHolder.ivUserIcon = (ImageView) convertView.findViewById(R.id.ivUserIcon);
+            viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.tvFullName = (TextView) convertView.findViewById(R.id.tvFullName);
+            viewHolder.tvLikes = (TextView) convertView.findViewById(R.id.tvLikesCount);
+            viewHolder.tvCreatedTime = (TextView) convertView.findViewById(R.id.tvRelativeTimeStamp);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        // Look up views to populate data
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
-        ImageView ivUserIcon = (ImageView) convertView.findViewById(R.id.ivUserIcon);
-        TextView tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
-        TextView tvFullName = (TextView) convertView.findViewById(R.id.tvFullName);
-        TextView tvLikes = (TextView) convertView.findViewById(R.id.tvLikesCount);
-        TextView tvCreatedTime = (TextView) convertView.findViewById(R.id.tvRelativeTimeStamp);
-
         // Event handlers
-        ivPhoto.setOnClickListener(new View.OnClickListener() {
+        viewHolder.ivPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 likePhoto();
             }
         });
-        tvLikes.setOnClickListener(new View.OnClickListener() {
+        viewHolder.tvLikes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 likePhoto();
             }
-
         });
 
-        // Insert the model data into each of the view items
+        // Populate the data into the template view using the data object
         String caption = "<b>" + photo.username + "</b><font color=#8e8e93> " + photo.caption + "</font>";
         String username = "<b>" + photo.username + "</b>";
         String likes = "<b>♥ " + NumberFormat.getInstance().format(photo.likesCount) + " likes</b>";
         String relativeDateTimeString = Utilities.getRelativeTimeSpanString_Formatted(photo.relativeTimeStamp);
 
-        tvCaption.setText(Html.fromHtml(caption));
-        tvUsername.setText(Html.fromHtml(username));
-        tvLikes.setText(Html.fromHtml(likes));
-        tvCreatedTime.setText(relativeDateTimeString);
-        tvFullName.setText(photo.fullName);
+        viewHolder.tvCaption.setText(Html.fromHtml(caption));
+        viewHolder.tvUsername.setText(Html.fromHtml(username));
+        viewHolder.tvLikes.setText(Html.fromHtml(likes));
+        viewHolder.tvCreatedTime.setText(relativeDateTimeString);
+        viewHolder.tvFullName.setText(photo.fullName);
 
         // Clear out the image views
-        ivPhoto.setImageResource(0);
-        ivUserIcon.setImageResource(0);
+        viewHolder.ivPhoto.setImageResource(0);
+        viewHolder.ivUserIcon.setImageResource(0);
 
         // Insert the image using picasso
-        Picasso.with(getContext()).load(photo.imageURL).placeholder(R.drawable.placeholder).into(ivPhoto);
-        Picasso.with(getContext()).load(photo.profilePictureURL).transform(new CircleTransform()).into(ivUserIcon);
+        Picasso.with(getContext()).load(photo.imageURL).placeholder(R.drawable.placeholder).into(viewHolder.ivPhoto);
+        Picasso.with(getContext()).load(photo.profilePictureURL).transform(new CircleTransform()).into(viewHolder.ivUserIcon);
 
+        // Return the completed view to render on screen
         return convertView;
     }
 
@@ -87,5 +93,16 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
                 "♥", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
+    }
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView tvCaption;
+        ImageView ivPhoto;
+        ImageView ivUserIcon;
+        TextView tvUsername;
+        TextView tvFullName;
+        TextView tvLikes;
+        TextView tvCreatedTime;
     }
 }
